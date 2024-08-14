87cc84455a6408d23a3af42b952693449e3863ac
Misha-25 <76957540+Misha-25@users.noreply.github.com>
2024-08-14 20:04:37 +0530

    14.08

---
+++ Misha/MakeTwoArrayEqual.java
@@ -0,0 +1,16 @@
+package Misha;
+
+import java.util.Arrays;
+
+public class MakeTwoArrayEqual {
+    public static boolean canbeEqual(int target[], int arr[]){
+        Arrays.sort(target);
+        Arrays.sort(arr);
+        return Arrays.equals(target, arr);
+    }
+    public static void main(String[] args) {
+        boolean solution =  canbeEqual(new int[]{1,2,3,4}, new int[]{2,4,1,11});
+        System.out.println(solution);
+    }
+    
+}
