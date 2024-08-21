6e0c90123e8630447b173b9f5d1b3ca0b3445e5f
Misha-25 <76957540+Misha-25@users.noreply.github.com>
2024-08-21 13:44:58 +0530

    triplet sums to given number in array. solved without declaring fucn as static.

---
+++ Misha/TripletSum.java
@@ -0,0 +1,22 @@
+package Misha;
+
+public class TripletSum {
+    boolean  findTriplet(int arr[], int sum){
+        for(int i=0; i<arr.length-2;i++){
+            for(int j=i+1;j<arr.length-1;j++){
+                for(int k=0;k<arr.length;k++){
+                    if(arr[i] + arr[j] + arr[k] == sum){
+                        System.out.println("Triplet is " + arr[i] + " ," + arr[j] + "," + arr[k]);
+                    }
+                }
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        TripletSum triplet = new TripletSum();
+        int arr[] = new int[]{1,4,45,6,10,8};
+        int sum = 22;
+      triplet.findTriplet( arr, sum);
+    }
+}
