da45888d01d65600276f781f8fa63bd3c2ac9c2f
Misha-25 <76957540+Misha-25@users.noreply.github.com>
2024-08-22 17:07:34 +0530

    .

--- Misha/TripletSum.java
+++ Misha/TripletSum.java
@@ -4,7 +4,7 @@ public class TripletSum {
     boolean  findTriplet(int arr[], int sum){
         for(int i=0; i<arr.length-2;i++){
             for(int j=i+1;j<arr.length-1;j++){
-                for(int k=0;k<arr.length;k++){
+                for(int k=j+1;k<arr.length;k++){
                     if(arr[i] + arr[j] + arr[k] == sum){
                         System.out.println("Triplet is " + arr[i] + " ," + arr[j] + "," + arr[k]);
                     }
