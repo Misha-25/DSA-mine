9c8dcb843e9717ff5fa5290b00fa8545338bbff0
Misha-25 <76957540+Misha-25@users.noreply.github.com>
2024-09-11 00:08:26 +0530

    11.se

---
+++ Misha/String/CharacterOccurence.java
@@ -0,0 +1,34 @@
+package Misha.String;
+
+public class CharacterOccurence {
+    public static void main(String[] args) {
+        
+    
+    String input = "aaabbbbbbccccccccccc";
+    int countA=0;
+    int countB=0;
+    int countC=0;
+    for(char ch : input.toCharArray()){
+        switch (ch) {
+            case 'a' :
+            countA++;
+            break;
+            case 'b' :
+            countB++;
+            break;
+            case 'c' :
+            countC++;
+                
+                break;
+        
+            default:
+                break;
+        }
+    }
+    System.out.println("occurence of a " + countA);
+    System.out.println("occurence of b " + countB);
+    System.out.println("occurence of c " + countC);
+    
+    
+}
+}
